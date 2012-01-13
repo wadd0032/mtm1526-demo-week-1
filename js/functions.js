5758f2f200b983637b29db5e4513860e7acfc342
wadd0032 <deannawadden@rogers.com>
2012-01-13 11:35:39 -0500

    Added a "Hello" message

--- js/functions.js
+++ js/functions.js
@@ -7,5 +7,11 @@ JavaScript multi-line comment
 // 'alert()' pops up a little window with an OK button.
 alert('This is an alert.');
 
-// 'prompt()' pops up a window and field for user to supply info.
-prompt('What is your name?');
+// 'prompt()' is for user input, has a text field for the user to enter some text.
+var name = prompt('What is your name?');
+
+// 'console.log()' writes out the content of a variable for debugging
+//identical to Actionscript's 'trace()'
+console.log(name);
+
+document.write('Hello, ' + name);
