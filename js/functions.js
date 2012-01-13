98a598e075f268cedb55e1ab0988a142376825d8
wadd0032 <deannawadden@rogers.com>
2012-01-13 11:24:45 -0500

    addend notation

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,6 @@ JavaScript multi-line comment
 
 // 'alert()' pops up a little window with an OK button.
 alert('This is an alert.');
+
+// 'prompt()' pops up a window and field for user to supply info.
+prompt('What is your name?');
