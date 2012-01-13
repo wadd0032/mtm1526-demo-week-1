e0fa9219e5d258a1c7639220f207d0e4296639dc
wadd0032 <deannawadden@rogers.com>
2012-01-13 11:20:35 -0500

    added notation

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+// JavaScript single line comment
+
+/*
+JavaScript multi-line comment
+*/
+
+// 'alert()' pops up a little window with an OK button.
+alert('This is an alert.');
